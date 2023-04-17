dcb450e3d764ddce7495efdedf5465138298bb41
Chatyusha <hirounicat76@gmail.com>
2023-04-17 19:22:39 +0900

    update clang installer

--- installerCollections/clang.sh
+++ installerCollections/clang.sh
@@ -8,7 +8,7 @@ cd llvm-project
 mkdir build
 cd build
 
-cmake -GNinja -DLLVM_ENABLE_PROJECTS=clang -DLLVM_ENABLE_RUNTIMES="libc;libcxx;libcxxabi" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$prefix/llvm" ../llvm
+cmake -GNinja -DLLVM_ENABLE_PROJECTS="clang;libc" -DLLVM_ENABLE_RUNTIMES="libc;libcxx;libcxxabi" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$prefix/llvm" ../llvm
 ninja -j $procs
 ninja install
 cd $prefix
